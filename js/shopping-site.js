672be80c29784f535e68a3c7e1b53795b2eb1ea4
EC2 Default User <ec2-user@ip-172-31-21-86.ec2.internal>
2018-12-27 06:10:05 +0000

    修正

--- js/shopping-site.js
+++ js/shopping-site.js
@@ -1,11 +1,40 @@
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel(
-        {
-            loop: true,
-            nav: false,
-            dots: true,
-            items: 1,
-            autoplay: true,
-        }
-    );
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        items: 1,
+        autoplay: true,
+    });
+});
+$(function() {
+    $(document).ready(function() {
+        $(".sp_menu_icon").click(function() {
+            $(this).next().slideToggle();
+        });
+        // <!-- サイドメニュー・アコーディオン -->
+        $(".pagelink_bar").click(function() {
+            $(".pagelink_li").slideToggle();
+            $(".pagelink_bar").toggleClass("active");
+        });
+        // <!-- サイドメニュー・アコーディオン -->
+        $(".categories_bar").click(function() {
+            $(".categories_li").slideToggle();
+            $(".categories_bar").toggleClass("active");
+        });
+        /*! ページトップへをヌルヌルさせる動きを実装 */
+        $(function() {
+            $(".pagetop a").click(function() {
+                $('html,body').animate({ scrollTop: $($(this).attr("href")).offset().top }, 'slow', 'swing');
+                return false;
+            })
+        });
+    });
+});
+// <!-- メインメニュー・スライドショー -->
+$(window).load(function() {
+    $("#list_img img").click(function() {
+        var img_src = $(this).attr("src");
+        $("#display_img img").attr("src", img_src);
+    });
 });
