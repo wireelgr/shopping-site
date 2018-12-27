c5d7d4870bdca3d4b14a6771d18e3c63003a12ba
EC2 Default User <ec2-user@ip-172-31-21-86.ec2.internal>
2018-12-27 07:46:11 +0000

    jquery修正

--- js/shopping-site.js
+++ js/shopping-site.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    // カルーセル
     $('.owl-carousel').owlCarousel({
         loop: true,
         nav: false,
@@ -6,35 +7,32 @@ $(document).ready(function() {
         items: 1,
         autoplay: true,
     });
-});
-$(function() {
-    $(document).ready(function() {
-        $(".sp_menu_icon").click(function() {
-            $(this).next().slideToggle();
-        });
-        // <!-- サイドメニュー・アコーディオン -->
-        $(".pagelink_bar").click(function() {
-            $(".pagelink_li").slideToggle();
-            $(".pagelink_bar").toggleClass("active");
-        });
-        // <!-- サイドメニュー・アコーディオン -->
-        $(".categories_bar").click(function() {
-            $(".categories_li").slideToggle();
-            $(".categories_bar").toggleClass("active");
-        });
-        /*! ページトップへをヌルヌルさせる動きを実装 */
-        $(function() {
-            $(".pagetop a").click(function() {
-                $('html,body').animate({ scrollTop: $($(this).attr("href")).offset().top }, 'slow', 'swing');
-                return false;
-            })
-        });
+    $(".sp_menu_icon").click(function() {
+        $(this).next().slideToggle();
+    });
+    //サイドメニュー・アコーディオン
+    $(".pagelink_bar").click(function() {
+        $(".pagelink_li").slideToggle();
+        $(".pagelink_bar").toggleClass("active");
+    });
+    //サイドメニュー・アコーディオン
+    $(".categories_bar").click(function() {
+        $(".categories_li").slideToggle();
+        $(".categories_bar").toggleClass("active");
+    });
+    /*! ページトップへをヌルヌルさせる動きを実装 */
+    $(function() {
+        $(".pagetop a").click(function() {
+            $('html,body').animate({ scrollTop: $($(this).attr("href")).offset().top }, 'slow', 'swing');
+            return false;
+        })
     });
 });
-// <!-- メインメニュー・スライドショー -->
+//メインメニュー・スライドショー
 $(window).load(function() {
     $("#list_img img").click(function() {
         var img_src = $(this).attr("src");
         $("#display_img img").attr("src", img_src);
     });
 });
+    
